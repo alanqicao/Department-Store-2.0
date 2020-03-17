dbd3ec941efa66ad6e0c2ca437ceafcd269d449c
Qi Cao <alanqicao@gmail.com>
2020-03-17 17:41:57 -0600

    first commit

---
+++ client/src/components/DepartmentView.js
@@ -0,0 +1,37 @@
+import React from "react";
+import axios from "axios";
+import { Button, Header, Segment, } from "semantic-ui-react";
+
+class DepartmentReview extends React.Component {
+  state = { department: {}, };
+
+  componentDidMount() {
+    axios.get(`/api/departments/${this.props.match.params.id}`)
+      .then( res => {
+        this.setState({ department: res.data, });
+      })
+  }
+
+
+  render() {
+    const { name, } = this.state.department;
+
+    return (
+      <div>
+        <Segment>
+          <Header as="h1">{ name }</Header>
+        </Segment>
+        <br />
+        <br />
+        <Button 
+          color="black" 
+          onClick={this.props.history.goBack}
+        >
+          Back
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default DepartmentReview;
---
+++ client/src/components/Departments.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Card, Header, Button, } from "semantic-ui-react";
+import { Link, } from "react-router-dom"
+import axios from "axios"
+
+class Departments extends React.Component {
+  state = { departments: [], };
+
+  componentDidMount() {
+    axios
+      .get("api/departments")
+      .then(res => {
+        this.setState({ departments: res.data, })
+      });
+  }
+
+
+  updateDepartments = (id) => {
+    console.log("update")
+    console.log(id)
+
+    axios.put(`/api/departments/${id}`)
+      .then( res => {
+        const departments = this.state.departments.map( t => {
+        if (t.id === id)
+          return res.data;
+        return t;
+      });
+      this.setState({ departments, });
+    })
+  }
+
+  deleteDepartment = (id) => {
+    console.log("delete!!!!!")
+    console.log(id)
+
+    axios.delete(`/api/departments/${id}`)
+      .then( res => {
+        const { departments, } = this.state;
+        this.setState({ departments: departments.filter(d => d.id !== id), })
+      })
+  }
+
+  renderDepartments = () => {
+    const { departments, } = this.state;
+
+    if (departments.length <= 0)
+      return <h2>No Departments</h2>
+    return departments.map(department => (
+      <Card>
+        <Card.Content>
+          <Card.Header>{department.name}</Card.Header>
+        </Card.Content>
+        <Card.Content extra>
+          <Button as={Link} to={`/departments/${department.id}`} color='blue'>
+            View
+          </Button>
+          <Button 
+          
+          onClick={() => this.deleteDepartment(department.id)} 
+          color='blue'>
+            Delete
+          </Button>
+          <Button 
+          
+          onClick={() => this.updateDepartments(department.id)} 
+          color='blue'>
+            Update
+          </Button>
+        </Card.Content>
+      </Card>
+    ))
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h1">Departments</Header>
+        <br />
+        <Button as={Link} color="blue" to="/departments/new">
+          Add Departments
+        </Button>
+        <br />
+        <br />
+        <Card.Group>
+          {this.renderDepartments()}
+        </Card.Group>
+      </div>
+    )
+  }
+}
+
+export default Departments;
+
+
