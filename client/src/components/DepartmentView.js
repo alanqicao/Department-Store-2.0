1bdbcb027ccaa367bb24cf27c1a7709570fe1bf4
Qi Cao <alanqicao@gmail.com>
2020-03-20 00:57:39 -0600

    itemForm

--- client/src/components/DepartmentView.js
+++ client/src/components/DepartmentView.js
@@ -3,16 +3,21 @@ import axios from "axios";
 import { Button, Header, Segment } from "semantic-ui-react";
 
 class DepartmentReview extends React.Component {
-  state = { department: {}, items: [] };
+  state = { department: [], items: [] };
 
   componentDidMount() {
     const department_id = this.props.match.params.id;
-    axios.get(`/api/departments/${department_id}`)
+    axios
+      .get(`/api/departments/${department_id}`)
       .then(res => {
-        const departmentData = res.data
-        axios.get(`/api/departments/${department_id}/items`)
+        const departmentData = res.data;
+        axios
+          .get(`/api/departments/${department_id}/items`)
           .then(res => {
-            this.setState({ department: departmentData, items: res.data });
+            this.setState({ 
+              department: departmentData, 
+              items: res.data 
+            });
 
           })
       })
@@ -32,7 +37,7 @@ class DepartmentReview extends React.Component {
           <Header as="h1">Comments 1st example</Header>
           {this.state.items.map(item => (
             <Segment>
-              <Header as="h3">{item.user}</Header>
+              <Header as="h3">{item.name}</Header>
               <p>{item.text}</p>
             </Segment>
           ))}
--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -2,41 +2,86 @@ import React, { Component } from "react";
 import { Form } from "semantic-ui-react";
 
 class ItemForm extends Component {
-  state = { name: "" };
+  state = { items:[], name: "", price:""};
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+  componentDidMount() {
+    const { itemId } = this.props;
+    axios.get(`/api/departments/${itemId}/items`).then(res => {
+      console.log(res);
+      this.setState({
+        items: res.data
+      });
+    });
+  }
 
-  handleSubmit = () => {
-    const { name, price } = this.state;
-    const { menuId } = this.props;
-    this.props.addItem(menuId, name, price);
-    this.setState({ name: "", price: "" });
-  };
+  deleteItem(id) {
+    const { departmentId } = this.props;
+    axios.delete(`/api/departments/${departmentId}/items/${id}`).then(()=>{
+      const newItems = this.state.items.filter( item => item.id != id)
+      this.setState({
+        items:newItems
+      })
+    })
 
+  }
+
+
+  handleChange =(e)=>{
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+  handleSubmit=(e)=>{
+    const { departmentId } = this.props;
+    axios.post(`/api/departments/${departmentId}/items`,{
+      name: this.state.name,
+      price:this.state.price
+    }).then((res)=>{
+      this.setState({
+        items:[res.data, ...this.state.items]
+      })
+    }).catch(err=>{
+
+    })
+    // this.setState({
+    //   [name]: value
+    // })
+  }
   render() {
-    const { name, price } = this.state;
-
+    const { name,  price} = this.state
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group>
-          <Form.Input
-            placeholder="Item Name"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Input
-            placeholder="Item Price"
-            name="price"
-            type="number"
-            value={price}
-            onChange={this.handleChange}
-          />
-          <Form.Button content="Submit" />
-        </Form.Group>
-      </Form>
+      <Segment>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="name"
+              name="name"
+              placeholder="name"
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+            <Form.Input
+              label="Add item"
+              name="price"
+              placeholder="item"
+              value={price}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Button color="blue">Submit</Form.Button>
+        </Form>
+        <Header as="h1">items 2nd example</Header>
+        {this.state.items.map(item => (
+          <Segment key={`item=${item.id}`}>
+            <Header as="h3">{item.name}</Header>
+            <p>{item.price}</p>
+            <Button onClick={() => this.deleteItem(item.id)} color='red'>delete</Button>
+          </Segment>
+        ))}
+      </Segment>
     );
   }
 }
